b915bdbe21fdd57101f72565ae8452127bd78287
Shreya Deshpande <70312732+iamShreyaD@users.noreply.github.com>
2026-07-29 21:48:47 +0530

    Create p1.py

---
+++ p1.py
@@ -0,0 +1,25 @@
+x = 1
+print(x)     #x=1
+x = 2
+y = x + 3
+print(x)      #x=2
+print (y)      #x=5
+
+print("My name is Shreya")
+print("Shreya is learning Python")
+print("Shreya wants to become Python expert")
+
+#to change the name you have to do it manually
+name = "Shreya"
+print("My name is", name)
+print(name, "is learning Python")
+print(name, "wants to become Python expert")
+
+
+# python executes code line-by-line
+# challenge print the folowing three lines and add a variable to make it dynamic
+
+user = "datawithbaraa"
+print("info@" + user + ".com")
+print("support@" + user + ".com") 
+print("www." + user + ".com")
