a77b11ba2aa5e7282b0e5b545b797e14170be063
Shreya Deshpande <70312732+iamShreyaD@users.noreply.github.com>
2026-08-02 15:45:51 +0530

    Challenge: Clean messy phone number

--- p6.py
+++ p6.py
@@ -1,5 +1,4 @@
-# Convert the messy phone number into a clean number format with only digits
-
+# Convert the messy phone number into a clean number format with only digits like "00491761234567"
 
 phone = "+49 (176) 123-4567"
-print(phone.replace("+", "").replace("(", "").replace(")", "") .replace("-", ""))
+print(phone.replace("+", "00").replace("(", "").replace(")", "") .replace("-", "").replace(" ", ""))
