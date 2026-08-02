74cafbe0233475a65b236ff0df45deaf18ffe86c
Shreya Deshpande <70312732+iamShreyaD@users.noreply.github.com>
2026-08-02 15:07:42 +0530

    Update p4.py

--- p4.py
+++ p4.py
@@ -79,3 +79,58 @@ Use only input(), print(), variables, type(), and basic arithmetic.
 No if statements.
 No loops.
 No functions.
+
+
+
+# inputs for user
+name = input("What is the name of the Student?: ")
+age = int(input("What is the age of the student?: "))
+height = round(float(input("What is the height of the student in centimeters?: ")), 1)
+per = round(float(input("What is the percentage that the student secured in the last exam?: ")), 1)
+subject = input("What is your favorite subject?: ")
+is_sports = input("Does the student play any sport? Answer in yes or no: ").strip().lower() == 'yes'
+no_sibs = int(input("How many siblings does the student have?: "))
+
+# print answers
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Height: {height} cm")
+print(f"Percentage is {per}%")
+print(f"Favorite Subject: {subject}")
+print(f"Plays sports: {is_sports}")
+print(f"Siblings: {no_sibs}")
+
+# data types
+print(f"Type of name: {type(name)}")
+print(f"Type of age: {type(age)}")
+print(f"Type of heigh: {type(height)}")
+print(f"Type of percentage: {type(per)}")
+print(f"Type of plays_sports: {type(is_sports)}")
+
+# creating variables
+birth_year = 2026 - age
+half_height = height / 2
+percentage_in_decimal = per / 100
+
+print(f"Birth year is {birth_year}")
+print(f"Half height is {half_height}")
+print(f"The percentage in decimal is {percentage_in_decimal}")
+
+# a = 10
+# b = 5.5
+# c = "20"
+# d = True
+
+# print(type(a + b)) = float
+# print(type(c)) = string
+# print(type(d)) = bool
+# print(type(a * 2)) = int
+# print(type(b / 2)) = float
+
+# Predict the output of these expressions:
+
+# print(bool(0)) = False
+# print(bool(1)) = True
+# print(bool("")) = False
+# print(bool("Python")) = True
+# print(bool(15)) = True
